6a2ed1820986cfc7eb104480903bbc7c738de1ec
Eduardo Júnior <junivico20@outlook.com>
2023-08-03 20:40:46 -0300

    feat: adicionando funcionabilidades ao projeto

---
+++ script.js
@@ -0,0 +1,60 @@
+const input = document.querySelector(".input-text")
+const listTaskComplete = document.querySelector(".list-item")
+
+let listItems = []
+
+function addNewTask() {
+
+  listItems.push({
+    task_item: input.value,
+    completed: false
+  })
+  input.value = ''
+
+  displayTask()
+}
+
+function displayTask() {
+  let newLi = ''
+
+  listItems.forEach((task, position) => {
+    newLi = newLi + `
+
+        <li class="item ${task.completed && "done"}">
+          <p>${task.task_item}</p>
+          <div class="img-item">
+            <img src="img/checked.png" alt="checked" onclick="checkItems(${position})" />
+            <img src="img/trash.png" alt="lixeira" onclick="removeItems(${position})" />
+          </div>
+        </li>
+        `
+  })
+
+  listTaskComplete.innerHTML = newLi 
+
+
+  localStorage.setItem('list', JSON.stringify(listItems))
+}
+
+function removeItems(position){
+  listItems.splice(position, 1)
+  displayTask()
+}
+
+function checkItems(position){
+  listItems[position].completed = !listItems[position].completed
+
+  displayTask()
+}
+
+function rechargeTask(){
+  const taskLocal = localStorage.getItem('list')
+
+  if(taskLocal){
+    listItems = JSON.parse(taskLocal)
+  }
+  
+  displayTask()
+}
+
+rechargeTask()
